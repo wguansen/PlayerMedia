ca91222f74eb8da4a3fbca03eb689a40d8ab7305
王冠森 <>
2020-10-29 17:11:28 +0800

    1

---
+++ Example/Pods/Target Support Files/PlayerMedia/PlayerMedia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PlayerMedia : NSObject
+@end
+@implementation PodsDummy_PlayerMedia
+@end
---
+++ Example/Pods/Target Support Files/Pods-PlayerMedia_Tests/Pods-PlayerMedia_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlayerMedia_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PlayerMedia_Tests
+@end
